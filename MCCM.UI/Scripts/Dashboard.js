666467bf2146c06c166062371d260adf1ba139bd
Jordan <jordanea02@gmail.com>
2020-09-29 17:50:43 -0600

    Tabs implementations and add modules

---
+++ MCCM.UI/Scripts/Dashboard.js
@@ -0,0 +1,7 @@
+﻿function changeVisiblePestannaBody(name) {
+    $("#pestanna-1-panel-body").css("visibility", "collapse");
+    $("#pestanna-2-panel-body").css("visibility", "collapse");
+    $("#reporte-panel-body").css("visibility", "collapse");
+
+    $('#' + name).css("visibility", "visible");
+}
