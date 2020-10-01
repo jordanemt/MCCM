b56e14890c11d0d72fc2c92436fb79f05f19cee8
Jordan <jordanea02@gmail.com>
2020-10-01 14:29:32 -0600

    Add select, dataRange and moment by cdm

--- MCCM.UI/App_Start/BundleConfig.cs
+++ MCCM.UI/App_Start/BundleConfig.cs
@@ -9,14 +9,10 @@ namespace MCCM.UI
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery.daterangepicker/date.js"));
+                        "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/moment").Include(
-                        "~/Scripts/moment.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
@@ -24,15 +20,12 @@ namespace MCCM.UI
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/bootstrap-select.min.js"));
+                      "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/font-awesome.min.css",
-                      "~/Content/jquery.daterangepicker/ui.daterangepicker.css",
-                      "~/Content/bootstrap-select.min.css"));
+                      "~/Content/font-awesome.min.css"));
         }
     }
 }
