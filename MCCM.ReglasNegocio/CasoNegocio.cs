f5ea8128f660b14bd872e71f444a57d2159fd433
Maikel <maikelmatamorosz@gmail.com>
2020-10-05 03:58:54 -0600

    CU Insertar Caso, Agregó Datos, Negocio y Controller para Caso

---
+++ MCCM.ReglasNegocio/CasoNegocio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MCCM.AccesoDatos;
+using MCCM.Entidad;
+using MCCM.Entidad.DTO;
+
+namespace MCCM.ReglasNegocio
+{
+    public class CasoNegocio
+    {
+        AccesoDatos.CasoDatos casoDatos = new AccesoDatos.CasoDatos();
+
+        public void InsertarCaso(TMCCM_Caso caso) {
+            casoDatos.InsertarCaso(caso);
+        }
+
+        public List<TMCCM_CasoDTO> ListarCasos() {
+            return casoDatos.ListarCasos();
+        }
+
+        public TMCCM_Caso ObtenerCasoPorID(int ID) {
+            return casoDatos.ObtenerCasoPorID(ID);
+        }
+
+    }
+}
---
+++ MCCM.UI/Scripts/js/Caso.js
@@ -0,0 +1,58 @@
+﻿$(document).ready(function () {
+    CargarCasos();
+
+});
+
+$(document).on("click",".caso",function () {
+
+    if ($(".card").hasClass('filaseleccionada')) {
+        $(".card").removeClass('filaseleccionada');
+        $(this).addClass('filaseleccionada');
+        alert($(this).attr('id'));
+    } else {
+        $(this).addClass('filaseleccionada');
+        alert($(this).attr('id'));
+    }
+});
+
+$("#FormCaso").submit(function (e) {
+    e.preventDefault();
+    var form = $(this);
+    alert(JSON.stringify(form.serializeArray()));
+    $.ajax({
+        type: "POST",
+        url: "/Caso/InsertarCaso",
+        data: form.serialize()
+
+    }).done(function (data) {
+        CargarCasos();
+    });
+});
+
+
+
+
+
+function CargarCasos() {
+    $.ajax({
+        type: "GET",
+        url: "/Caso/ListarCasos"
+    }).done(function (data) {
+        alert(data);
+        let casos = new Array();
+        casos = JSON.parse(data);
+        for (let i = 0; i < casos.length; i++) {
+            $("#casos-body").append(
+                '<div class="card caso" id="' + casos[i].TN_ID_Caso +'" style="height:10em;">'+
+                '<div class="card-header"><div>Caso #' + casos[i].TN_ID_Caso + '</div>'+
+                '<a href="#" class="ojito" id="' + casos[i].TN_ID_Caso+'"><span><i class="fa fa-eye" aria-hidden="true"></i></span></a></div >'+
+                        '<div class="card-body" style="padding:0px!important">'+
+                            '<h5><small>Nombre:'+ casos[i].TC_Nombre_Caso +'</small></h5>'+
+                            '<h5><small>Fecha: ' + casos[i].TF_Fecha +'</small></h5>'+
+                            '<h5><small>Delito: ' + casos[i].TC_Delito +'</small></h5>'+
+                        '</div>'+
+                    '</div>'
+            );
+        }
+    });
+}
