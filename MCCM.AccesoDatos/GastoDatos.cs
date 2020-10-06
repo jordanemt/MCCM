36420d66d1dbae6665691b9b18270ef2dc971550
Jordan <jordanea02@gmail.com>
2020-10-06 17:10:39 -0600

    Add GastoDatos

---
+++ MCCM.AccesoDatos/GastoDatos.cs
@@ -0,0 +1,54 @@
+﻿using MCCM.Entidad;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCCM.AccesoDatos
+{
+    class GastoDatos
+    {
+        public IEnumerable<TMCCM_Gasto> GetAll() 
+        {
+            using (var context = new MCCMEntities())
+            {
+                return context.TMCCM_Gasto.ToList();
+            }
+        }
+
+        public TMCCM_Gasto Insert(TMCCM_Gasto gasto) 
+        {
+            using (var context = new MCCMEntities())
+            {
+                gasto.TB_Eliminado = true;
+                gasto.TF_Fecha = DateTime.Now;
+                TMCCM_Gasto newGasto = context.TMCCM_Gasto.Add(gasto);
+                context.SaveChanges();
+                return newGasto;
+            }
+        }
+
+        public void Update(TMCCM_Gasto gasto)
+        {
+            using (var context = new MCCMEntities())
+            {
+                gasto.TF_Fecha = DateTime.Now;
+                context.Entry(gasto).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        public void Delete(TMCCM_Gasto gasto)
+        {
+            using (var context = new MCCMEntities())
+            {
+                gasto.TB_Eliminado = false;
+                gasto.TF_Fecha = DateTime.Now;
+                context.TMCCM_Gasto.Add(gasto);
+                context.SaveChanges();
+            }
+        }
+    }
+}
