c89804fc39fb0f9b88fd76bcb4f72d204324915e
valerialeiva <Suisivale30>
2020-10-08 12:51:26 -0600

    Entidad Persona

---
+++ MCCM.AccesoDatos/EntidadPersonaDatos.cs
@@ -0,0 +1,42 @@
+﻿using MCCM.Entidad;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCCM.AccesoDatos
+{
+   public class EntidadPersonaDatos
+    {
+        public void InsertarEntidadPersona(TMCCM_Entidad_Persona entidadPersona)
+        {
+            using (var context = new MCCMEntities())
+            {
+
+                entidadPersona.TB_Eliminado = true;
+                entidadPersona.TF_Fecha_Creacion = DateTime.Now;
+
+                context.TMCCM_Entidad_Persona.Add(entidadPersona);
+                context.SaveChanges();
+            }
+        }
+
+        public static byte[] GetPhoto(string filePath)
+        {
+            FileStream stream = new FileStream(
+                filePath, FileMode.Open, FileAccess.Read);
+            BinaryReader reader = new BinaryReader(stream);
+
+            byte[] photo = reader.ReadBytes((int)stream.Length);
+
+            reader.Close();
+            stream.Close();
+
+            return photo;
+        }
+
+
+    }
+}
---
+++ MCCM.UI/Controllers/E_PersonaController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MCCM.Entidad.DTO;
+using Newtonsoft.Json;
+
+namespace MCCM.UI.Controllers
+{
+    public class E_PersonaController : Controller
+    {
+        // GET: E_Persona
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public String Insertar_E_Persona(TMCCM_EntidadPersonaDTO entidadPersonaDTO)
+        {
+            
+            return "S";
+        }
+
+    }
+}
