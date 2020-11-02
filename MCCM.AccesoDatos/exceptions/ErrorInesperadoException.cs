4b34468bec1bb7aec25e9706f9a601d97b17ac6a
Jordan Méndez <jordanemendezt@gmail.com>
2020-11-02 17:29:28 -0600

    Exceptions manager

---
+++ MCCM.AccesoDatos/exceptions/ErrorInesperadoException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+
+namespace MCCM.AccesoDatos.exceptions
+{
+    class ErrorInesperadoException : Exception
+    {
+        public ErrorInesperadoException() : base("Error inesperado") 
+        {
+            HttpContext.Current.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+        }
+    }
+}
---
+++ MCCM.AccesoDatos/exceptions/ExceptionHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.Entity.Core;
+using System.Net;
+using System.Web;
+
+namespace MCCM.AccesoDatos.exceptions
+{
+    class ExceptionHandler
+    {
+        public static Exception Handle(Exception e)
+        {
+            Type typeException = e.GetType();
+            
+            if (typeof(EntityException).IsAssignableFrom(typeException)) {
+                HttpContext.Current.Response.StatusCode = (int) HttpStatusCode.BadGateway;
+                return new ArgumentException("Error con la base de datos", e); 
+            }
+
+            return new ErrorInesperadoException();
+        }
+    }
+}
