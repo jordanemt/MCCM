46f1ef96a52a11a203ac903ac9c30aa698f79827
Jordan Méndez <jordanemendezt@gmail.com>
2020-11-02 18:13:01 -0600

    Error using

--- MCCM.ReglasNegocio/EventoNegocio.cs
+++ MCCM.ReglasNegocio/EventoNegocio.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity.Core.Objects;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MCCM.AccesoDatos;
+﻿using System.Collections.Generic;
 using MCCM.Entidad;
 using MCCM.Entidad.DTO;
 
